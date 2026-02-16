7065698d40a4c44a3b37b35c204a39a16176c9e0
Pilgrimage World <pilgrimageworld.com@gmail.com>
2026-02-16 10:59:54 +0530

    Delete All The Files and Start with New...

--- data_structures/sets.py
+++
@@ -1,6 +0,0 @@
-set = set()
-
-set = {20, 3, 4, 4, 5}
-
-set.union(set)
-print(set)
--- other_stuff/lambda.py
+++
@@ -1,8 +0,0 @@
-square = lambda x: x * x
-
-# print(square(8))
-
-def apply(fx, value):
-    return value * fx(value)
-
-print(apply(lambda x: x*x, 6))
